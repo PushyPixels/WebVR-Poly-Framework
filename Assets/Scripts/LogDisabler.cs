4fa583c545226669ed0947b38d42eef70ca1b062
Maximilian Maroe <drakfyre@gmail.com>
2019-05-26 01:44:44 -0700

    Disabling logger for a framerate test

---
+++ Assets/Scripts/LogDisabler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogDisabler : MonoBehaviour
+{
+    private static ILogger logger = Debug.unityLogger;
+    private static string kTAG = "MyGameTag";
+
+    void Start()
+    {
+        logger.logEnabled = Debug.isDebugBuild;
+
+        logger.Log(kTAG, "This log will be displayed only in debug build");
+    }
+}
